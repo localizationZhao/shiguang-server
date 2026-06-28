eff7502341050adf0481a0763d22f5af3f41dec6
赵万里 <zwl666268@qq.com>
2026-06-28 09:02:58 +0000

    lecture3_课堂练习二_事件绑定与setData
    Signed-off-by: 赵万里 <zwl666268@qq.com>

---
+++ lecture3/lecture3_课堂练习一和二/lecture3_课堂练习二_事件绑定与setData/app.js
@@ -0,0 +1,18 @@
+// app.js - 小程序入口文件
+App({
+  onLaunch: function() {
+    // 小程序启动时触发，全局只触发一次
+    console.log('App Launch');
+  },
+  onShow: function() {
+    // 小程序启动或从后台进入前台时触发
+    console.log('App Show');
+  },
+  onHide: function() {
+    // 小程序从前台进入后台时触发
+    console.log('App Hide');
+  },
+  globalData: {
+    userInfo: null
+  }
+});
